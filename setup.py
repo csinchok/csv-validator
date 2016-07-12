38c4db3b7537c8e6af78e837924ec91d4e554be0
Chris Sinchok <chris@sinchok.com>
2016-07-12 15:17:05 -0500

    what is happening to me today

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Chris Sinchok',
     author_email='chris@sinchok.com',
     name='csv-validator',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/csinchok/csv-validator',
     packages=['csv_validator'],
     test_suite='tests',
