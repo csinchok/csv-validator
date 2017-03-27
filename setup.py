3caa16ebc2eaa5374cf601af1448b49fa5d4bcb9
Chris Sinchok <chris@sinchok.com>
2017-03-27 11:53:43 -0500

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Chris Sinchok',
     author_email='chris@sinchok.com',
     name='csv-validator',
-    version='0.1.9',
+    version='0.1.10',
     url='https://github.com/csinchok/csv-validator',
     packages=['csv_validator'],
     test_suite='tests',
